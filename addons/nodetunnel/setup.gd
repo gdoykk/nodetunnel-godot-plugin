a0806d6ca356bd9b67a41508a9dfc5531637192a
Gdoykk <37079672+gdoykk@users.noreply.github.com>
2026-07-14 20:03:27 +0000

    Remove update checker
    Deletes the updater/ directory and its GitHub-release-API update
    check entirely, along with the now-dead references in setup.gd.

--- addons/nodetunnel/setup.gd
+++ addons/nodetunnel/setup.gd
@@ -1,14 +1,7 @@
 @tool
 extends EditorPlugin
 
-var update_check = preload("updater/update_check.gd").new()
-
 func _enter_tree():
-	# Update check disabled: it makes an outbound network request to
-	# GitHub's API every time the editor loads this plugin. Uncomment
-	# below to re-enable it.
-	# add_child(update_check)
-	# update_check.check_update(get_plugin_version())
 	pass
 
 func _exit_tree():
